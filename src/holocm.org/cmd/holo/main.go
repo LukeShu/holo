d13b8f42bbdab282b2df3aeafd104c030f38f69e
Luke Shumaker <lukeshu@sbcglobal.net>
2017-01-11 00:56:06 -0500

    holo: move argument parsing into impl

--- src/holocm.org/cmd/holo/main.go
+++ src/holocm.org/cmd/holo/main.go
@@ -22,13 +22,10 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 
 	"holocm.org/cmd/holo/impl"
-	"holocm.org/cmd/holo/output"
 	"holocm.org/lib/holo"
 	"holocm.org/plugins/externalplugin"
 )
@@ -51,139 +48,5 @@ func main() {
 		return plugin, nil
 	}
 
-	Main(rootDir, version, getPlugin)
-}
-
-func Main(rootDir, version string, getPlugin impl.PluginGetter) {
-	const (
-		optionApplyForce = iota
-		optionScanShort
-		optionScanPorcelain
-	)
-
-	var runtimeManager *impl.RuntimeManager
-
-	help := func(w io.Writer) {
-		program := os.Args[0]
-		fmt.Fprintf(w, "Usage: %s <operation> [...]\nOperations:\n", program)
-		fmt.Fprintf(w, "    %s apply [-f|--force] [selector ...]\n", program)
-		fmt.Fprintf(w, "    %s diff [selector ...]\n", program)
-		fmt.Fprintf(w, "    %s scan [-s|--short|-p|--porcelain] [selector ...]\n", program)
-		fmt.Fprintf(w, "    %s --help\n", program)
-		fmt.Fprintf(w, "    %s --version\n", program)
-		fmt.Fprintf(w, "\nSee `man 8 holo` for details.\n")
-	}
-
-	exit := func(code int) {
-		if runtimeManager != nil {
-			runtimeManager.Close()
-		}
-		os.Exit(code)
-	}
-
-	// a command word must be given as first argument
-	if len(os.Args) < 2 {
-		help(os.Stderr)
-		exit(2)
-	}
-
-	//check that it is a known command word
-	var command func([]*impl.EntityHandle)
-	knownOpts := make(map[string]int)
-	options := make(map[int]bool)
-	switch os.Args[1] {
-	case "apply":
-		knownOpts = map[string]int{"-f": optionApplyForce, "--force": optionApplyForce}
-		command = func(e []*impl.EntityHandle) {
-			pidFile := impl.AcquirePidFile(filepath.Join(rootDir, "run/holo.pid"))
-			if pidFile == nil {
-				exit(255)
-			}
-			impl.CommandApply(e, options[optionApplyForce])
-			pidFile.Release()
-		}
-	case "diff":
-		command = impl.CommandDiff
-	case "scan":
-		knownOpts = map[string]int{
-			"-s": optionScanShort, "--short": optionScanShort,
-			"-p": optionScanPorcelain, "--porcelain": optionScanPorcelain,
-		}
-		command = func(e []*impl.EntityHandle) {
-			impl.CommandScan(e, options[optionScanPorcelain], options[optionScanShort])
-		}
-	case "version", "--version":
-		fmt.Println(version)
-		exit(0)
-	case "help", "--help":
-		help(os.Stdout)
-		exit(0)
-	default:
-		help(os.Stderr)
-		exit(2)
-	}
-
-	// load configuration
-	configReader, err := impl.NewConfigReader(rootDir)
-	if err != nil {
-		output.Errorf(output.Stderr, "%s", err.Error())
-		exit(255)
-	}
-	config, err := impl.ReadConfig(configReader)
-	if err != nil {
-		output.Errorf(output.Stderr, "%s", err.Error())
-		exit(255)
-	}
-
-	// load plugins
-	runtimeManager, err = impl.NewRuntimeManager(rootDir)
-	if err != nil {
-		exit(255)
-	}
-	plugins := runtimeManager.GetPlugins(config.Plugins, getPlugin)
-	if plugins == nil {
-		// some fatal error occurred - it was already
-		// reported, so just exit
-		exit(255)
-	}
-
-	// parse command line -- classify each argument as either a
-	// selector-string or an option-flag.
-	args := os.Args[2:]
-	selectors := make(map[string]bool)
-	for _, arg := range args {
-		// either it's a known option for this subcommand...
-		if value, ok := knownOpts[arg]; ok {
-			options[value] = true
-		} else { // ...or it must be a selector
-			selectors[arg] = false
-		}
-	}
-
-	// ask all plugins to scan for entities
-	entities, err := impl.GetAllEntities(plugins)
-	if err != nil {
-		output.Errorf(output.Stderr, "%s", err.Error())
-		exit(255)
-	}
-	if len(selectors) > 0 {
-		entities = impl.FilterEntities(entities, selectors)
-	}
-
-	// Were there unrecognized selectors?
-	hasUnrecognizedArgs := false
-	for selector, recognized := range selectors {
-		if !recognized {
-			fmt.Fprintf(os.Stderr, "Unrecognized argument: %s\n", selector)
-			hasUnrecognizedArgs = true
-		}
-	}
-	if hasUnrecognizedArgs {
-		exit(255)
-	}
-
-	//execute command
-	command(entities)
-
-	exit(0)
+	impl.Main(rootDir, version, getPlugin)
 }
