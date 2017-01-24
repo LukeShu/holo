3ac31cf6c5b92cd47d2fb0b817c1f79360c65fe7
Luke Shumaker <lukeshu@sbcglobal.net>
2017-01-24 04:06:08 -0500

    holo-files/impl/applyNonOrphan: Re-organize. Fixes https://github.com/holocm/holo/issues/24

--- src/holo-files/impl/targetfile_apply_nonorphan.go
+++ src/holo-files/impl/targetfile_apply_nonorphan.go
@@ -34,47 +34,17 @@ import (
 //This includes taking a copy of the target base if necessary, applying all
 //repository entries, and saving the result in the target path with the correct
 //file metadata.
-func (target *TargetFile) applyNonOrphan(withForce bool) (skipReport bool, err error) {
+func (target *TargetFile) applyNonOrphan(haveForce bool) (skipReport bool, err error) {
 	//determine the related paths
 	targetPath := target.PathIn(common.TargetDirectory())
 	targetBasePath := target.PathIn(common.TargetBaseDirectory())
 
-	//step 1: will only apply targets if:
-	//option 1: there is a manageable file in the target location (this target
-	//file is either the target base from the application package or the
-	//product of a previous Apply run)
-	//option 2: the target file was deleted, but we have a target base that we
-	//can start from
-	needForcefulReprovision := false
-	targetExists := common.IsManageableFile(targetPath)
-	if !targetExists {
-		if !common.IsManageableFile(targetBasePath) {
-			return false, errors.New("skipping target: not a manageable file")
-		}
-		if withForce {
-			needForcefulReprovision = true
-		} else {
-			return false, ErrNeedForceToRestore
-		}
-	}
-
-	//step 2: if we don't have a target base yet, the file at targetPath *is*
-	//the targetBase which we have to copy now
-	if !common.IsManageableFile(targetBasePath) {
-		targetBaseDir := filepath.Dir(targetBasePath)
-		err := os.MkdirAll(targetBaseDir, 0755)
-		if err != nil {
-			return false, fmt.Errorf("Cannot create directory %s: %s", targetBaseDir, err.Error())
-		}
-
-		err = common.CopyFile(targetPath, targetBasePath)
-		if err != nil {
-			return false, fmt.Errorf("Cannot copy %s to %s: %s", targetPath, targetBasePath, err.Error())
-		}
-	}
-
-	//step 3: check if a system update installed a new version of the stock
-	//configuration
+	// step 1: Check if a system update installed a new version of
+	// the stock configuration
+	//
+	// This has to come first because it might shuffle some files
+	// around, and if we do anything else first, we might end up
+	// stat()ing the wrong file.
 	updatedTBPath, reportedTBPath, err := platform.Implementation().FindUpdatedTargetBase(targetPath)
 	if err != nil {
 		return false, err
@@ -89,12 +59,55 @@ func (target *TargetFile) applyNonOrphan(withForce bool) (skipReport bool, err e
 		_ = os.Remove(updatedTBPath) //this can fail silently
 	}
 
-	//step 4: apply the repo files *if* the version at targetPath is the one
-	//installed by the package (which can be found at targetBasePath); complain if
-	//the user made any changes to config files governed by holo (this check is
-	//overridden by the --force option)
+	// step 2: Load the current version into memory.
+	needForcefulReprovision := false
+	targetBuffer, err := common.NewFileBuffer(targetPath)
+	if os.IsNotExist(err) {
+		targetBuffer, err = common.NewFileBuffer(targetBasePath)
+		targetBuffer.Path = targetPath
+		if err != nil {
+			return false, errors.New("skipping target: not a manageable file")
+		}
+		if !haveForce {
+			return false, ErrNeedForceToRestore
+		}
+		needForcefulReprovision = true
+	}
+	if err != nil {
+		return false, errors.New("skipping target: not a manageable file")
+	}
 
-	//load the last provisioned version
+	// step 3: Load the base version into memory.
+	baseBuffer, err := common.NewFileBuffer(targetBasePath)
+	baseBuffer.Path = targetPath
+	if os.IsNotExist(err) {
+		// if we don't have a target base yet, the file at
+		// targetPath *is* the targetBase; which we have to
+		// copy now
+		targetBaseDir := filepath.Dir(targetBasePath)
+		err = os.MkdirAll(targetBaseDir, 0755)
+		if err != nil {
+			return false, fmt.Errorf("Cannot create directory %s: %s", targetBaseDir, err.Error())
+		}
+
+		err = targetBuffer.Write(targetBasePath)
+		if err != nil {
+			return false, fmt.Errorf("Cannot copy %s to %s: %s", targetPath, targetBasePath, err.Error())
+		}
+		baseBuffer = targetBuffer
+	}
+	if err != nil {
+		return false, err
+	}
+
+	// step 4: apply the repo files (in memory)
+
+	buffer, err := target.Render(baseBuffer)
+	if err != nil {
+		return false, err
+	}
+
+	// step 5: Load the last-provisioned version into memory.
 	lastProvisionedPath := target.PathIn(common.ProvisionedDirectory())
 	lastProvisionedBuffer, err := common.NewFileBuffer(lastProvisionedPath)
 	lastProvisionedBuffer.Path = targetPath
@@ -103,72 +116,52 @@ func (target *TargetFile) applyNonOrphan(withForce bool) (skipReport bool, err e
 		return false, err
 	}
 
-	//compare it against the target version (which must exist at this point
-	//unless we are using --force)
-	if targetExists && haveLastProvisionedBuffer {
-		targetBuffer, err := common.NewFileBuffer(targetPath)
-		targetBuffer.Path = targetPath
+	// step 6: save to the filesystem
+	//
+	// we have 2 files to hit:
+	//  - the last-provisioned file
+	//  - the actual file
+
+	// last-provisioned file
+	if buffer != lastProvisionedBuffer || !haveLastProvisionedBuffer {
+		provisionedDir := filepath.Dir(lastProvisionedPath)
+		err = os.MkdirAll(provisionedDir, 0755)
+		if err != nil {
+			return false, fmt.Errorf("Cannot write %s: %s", lastProvisionedPath, err.Error())
+		}
+		err = buffer.Write(lastProvisionedPath)
 		if err != nil {
 			return false, err
 		}
-		if targetBuffer != lastProvisionedBuffer {
-			if withForce {
-				needForcefulReprovision = true
-			} else {
+	}
+
+	// actual file
+	if buffer != targetBuffer || needForcefulReprovision {
+		if haveLastProvisionedBuffer && targetBuffer != lastProvisionedBuffer {
+			if !haveForce {
 				return false, ErrNeedForceToOverwrite
 			}
 		}
-	}
-
-	//render desired state of target file
-	buffer, err := target.Render()
-	if err != nil {
-		return false, err
-	}
-
-	//don't do anything more if nothing has changed and the target file has not been touched
-	if !needForcefulReprovision && haveLastProvisionedBuffer {
-		if buffer == lastProvisionedBuffer {
-			//since we did not do anything, don't report this
-			return true, nil
+		// Do the $target.holonew -> $target shuffle so that
+		// $target is updated atomically (to ensure that there
+		// is always a valid file at $target)
+		err = buffer.Write(targetPath + ".holonew")
+		if err != nil {
+			return false, err
 		}
+		err = os.Rename(targetPath+".holonew", targetPath)
+		if err != nil {
+			return false, err
+		}
+		return false, nil
+	} else {
+		return true, nil
 	}
-
-	//save a copy of the provisioned config file to check for manual
-	//modifications in the next Apply() run
-	provisionedDir := filepath.Dir(lastProvisionedPath)
-	err = os.MkdirAll(provisionedDir, 0755)
-	if err != nil {
-		return false, fmt.Errorf("Cannot write %s: %s", lastProvisionedPath, err.Error())
-	}
-	err = buffer.Write(lastProvisionedPath)
-	if err != nil {
-		return false, err
-	}
-	err = common.ApplyFilePermissions(targetBasePath, lastProvisionedPath)
-	if err != nil {
-		return false, err
-	}
-
-	//write the result buffer to the target location and copy
-	//owners/permissions from target base to target file
-	newTargetPath := targetPath + ".holonew"
-	err = buffer.Write(newTargetPath)
-	if err != nil {
-		return false, err
-	}
-	err = common.ApplyFilePermissions(targetBasePath, newTargetPath)
-	if err != nil {
-		return false, err
-	}
-	//move $target.holonew -> $target atomically (to ensure that there is
-	//always a valid file at $target)
-	return false, os.Rename(newTargetPath, targetPath)
 }
 
 //Render applies all the repo files for this TargetFile onto the target base.
-func (t *TargetFile) Render() (common.FileBuffer, error) {
-	//check if we can skip any application steps
+func (t *TargetFile) Render(baseBuffer common.FileBuffer) (common.FileBuffer, error) {
+	// Optimization: check if we can skip any application steps
 	firstStep := 0
 	repoEntries := t.RepoEntries()
 	for idx, repoFile := range repoEntries {
@@ -176,26 +169,11 @@ func (t *TargetFile) Render() (common.FileBuffer, error) {
 			firstStep = idx
 		}
 	}
+	repoEntries = repoEntries[firstStep:]
 
-	//load the target base into a buffer as the start for the application
-	//algorithm
-	targetBasePath := t.PathIn(common.TargetBaseDirectory())
-	targetPath := t.PathIn(common.TargetDirectory())
-	var (
-		buffer common.FileBuffer
-		err    error
-	)
-	buffer, err = common.NewFileBuffer(targetBasePath)
-	buffer.Path = targetPath
-	if err != nil {
-		return common.FileBuffer{}, err
-	}
-
-	//apply all the applicable repo files in order (starting from the first one
-	//that matters)
-	if firstStep > 0 {
-		repoEntries = repoEntries[firstStep:]
-	}
+	//apply all the applicable repo files in order
+	buffer := baseBuffer
+	var err error
 	for _, repoFile := range repoEntries {
 		buffer, err = repoFile.ApplyTo(buffer)
 		if err != nil {
